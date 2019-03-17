edf35ce799d90433eb523db1252395f2f6513f46
redradist <redradist@gmail.com>
2019-03-17 14:56:24 +0200

    Changes from Lesson_2: Created simple Spring Application using spring.xml

--- comyetspringcore/src/main/java/com/learning/spring/core/App.java
+++ comyetspringcore/src/main/java/com/learning/spring/core/App.java
@@ -2,10 +2,18 @@ package com.learning.spring.core;
 
 import com.learning.spring.core.beans.Client;
 import com.learning.spring.core.loggers.ConsoleEventLogger;
+import org.omg.CORBA.portable.ApplicationException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     private Client client;
     private ConsoleEventLogger eventLogger;
+
+    private App(final Client client, final ConsoleEventLogger eventLogger) {
+        this.client = client;
+        this.eventLogger = eventLogger;
+    }
 
     void logEvent(final String msg) {
         final String message = msg.replaceAll(client.getId(), client.getFullName());
@@ -13,11 +21,10 @@ public class App {
     }
 
     public static void main(String[] args) {
-        App app = new App();
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("spring.xml");
 
-        app.client = new Client("1", "John Smith");
-        app.eventLogger = new ConsoleEventLogger();
-
+        App app = (App) applicationContext.getBean("app");
         app.logEvent("Some event for use 1");
     }
 }
